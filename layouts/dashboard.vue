3d5a129d777dd144775ae98fda4c2673c945da4c
Lucas Josino <contact@lucasjosino.com>
2025-05-29 11:53:32 -0300

    feat: add default and dashboard layouts

---
+++ layouts/dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard">
+    <DashboardHeader />
+    <main>
+      <div class="dashboard-container">
+        <div class="dashboard-content">
+          <slot />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100%;
+  background-color: var(--color-surface);
+  margin: 0;
+  padding: 0;
+}
+
+.dashboard {
+  display: flex;
+}
+
+.dashboard-container {
+  height: 100%;
+  width: 100%;
+}
+
+.dashboard-content {
+  height: 100%;
+  width: 100%;
+  max-width: var(--max-width);
+  background-color: white;
+  border-radius: var(--border-radius-md);
+  margin: 0 auto;
+  padding: var(--width-padding);
+  padding-top: calc(var(--header-height) + 1rem);
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style>
+main {
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
